9f2cde60f3cb75353540cf508e84abb26fddf014
Admin <admin@Admins-MBP.lan>
2025-01-09 15:11:09 +0530

    usb receiver not exported

--- libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
+++ libuvccamera/src/main/java/com/serenegiant/usb/USBMonitor.java
@@ -179,7 +179,11 @@ public final class USBMonitor {
 				final IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
 				// ACTION_USB_DEVICE_ATTACHED never comes on some devices so it should not be added here
 				filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-				context.registerReceiver(mUsbReceiver, filter);
+				if (Build.VERSION.SDK_INT >= 34 && context.getApplicationInfo().targetSdkVersion >= 34) {
+					context.registerReceiver(mUsbReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
+				} else {
+					context.registerReceiver(receiver, filter);
+				}
 			}
 			// start connection check
 			mDeviceCounts = 0;
